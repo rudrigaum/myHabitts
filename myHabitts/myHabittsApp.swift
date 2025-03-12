9d211af4db3c1a580d98b4c7d19a42810088f584
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-03-12 12:30:00 -0300

    Initial Commit

---
+++ myHabitts/myHabittsApp.swift
@@ -0,0 +1,17 @@
+//
+//  myHabittsApp.swift
+//  myHabitts
+//
+//  Created by Rodrigo Cerqueira Reis on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct myHabittsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
