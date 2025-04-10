ef72dbc23506b9944ab636e4cd83252ae310b103
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-04-10 15:20:48 -0300

    add fields to signUpView

--- myHabitts/SignUp/View/SignUpView.swift
+++ myHabitts/SignUp/View/SignUpView.swift
@@ -8,10 +8,89 @@
 import SwiftUI
 
 struct SignUpView: View {
+    
+    @State var fullName = ""
+    @State var email = ""
+    @State var password = ""
+    @State var document = ""
+    @State var phone = ""
+    @State var birthday = ""
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            ScrollView(showsIndicators: false) {
+                VStack(alignment: .center){
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Register")
+                            .foregroundStyle(.black)
+                            .font(Font.system(.title).bold())
+                            .padding()
+                        fullNameTextField
+                        emailTextField
+                        passwordTextField
+                        documentTextField
+                        phoneTextField
+                        birthdayTextField
+                        registerButton
+                    }
+                    Spacer()
+                }.padding(.horizontal, 8)
+            }.padding()
+        }
     }
 }
+
+
+extension SignUpView {
+    var fullNameTextField: some View {
+        TextField("", text: $fullName)
+            .border(Color.black)
+    }
+}
+extension SignUpView {
+    var emailTextField: some View {
+        TextField("", text: $email)
+            .border(Color.black)
+    }
+}
+
+extension SignUpView {
+    var passwordTextField: some View {
+        SecureField("", text: $password)
+            .border(Color.black)
+    }
+}
+
+extension SignUpView {
+    var documentTextField: some View {
+        SecureField("", text: $document)
+            .border(Color.black)
+    }
+}
+
+extension SignUpView {
+    var phoneTextField: some View {
+        SecureField("", text: $phone)
+            .border(Color.black)
+    }
+}
+
+extension SignUpView {
+    var birthdayTextField: some View {
+        SecureField("", text: $birthday)
+            .border(Color.black)
+    }
+}
+
+
+extension SignUpView {
+    var registerButton: some View {
+        Button("Register") {
+        }
+    }
+}
+    
 
 #Preview {
     SignUpView()
